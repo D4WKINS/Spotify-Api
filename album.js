e3b2e75815a3a028fa3c5befb6d7d43f61516eec
L-Greco <makaronaskonstandinos@gmail.com>
2021-04-16 12:36:13 +0300

    fetcched the data

--- album.js
+++ album.js
@@ -1,8 +1,4 @@
-
-
-
-
-
+//  PREVIOUS SCRIPT FROM HERE
 // let parentDivs = document.querySelectorAll('body > div > div > div > div.col-12.col-md-8 > div')
 // // console.log(parentDivs[0].firstElementChild.innerHTML)
 
@@ -23,3 +19,21 @@
 //         x.onmouseout = changeToNote
 //     }
 // )
+//  PREVIOUS SCRIPT ENDS HERE
+
+// NEW SCRIPT STARTS HERE
+let albumId = 75621062
+let albumTracks = []
+
+window.onload = () => {
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${albumId}`)
+        .then(res => res.json())
+        .then(album => {
+            console.log(album)
+            albumTracks = album.tracks.data
+            console.log("This is the array of tracks", albumTracks)
+
+
+        })
+        .catch(err => console.log(err))
+}
